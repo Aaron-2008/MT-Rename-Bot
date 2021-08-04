03c3fbafb77d90ee310ee1f7b021dae9babe412b
Aaron Shaji <86790904+Aaron-2008@users.noreply.github.com>
2021-08-04 06:17:53 +0000

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,7 +1,7 @@
 class Translation(object):
     START_TEXT = """ <b> 🙋‍♂️ Hi {} , I'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail support!💯</b> \n
 
-<b>Bot Maintained By: @Mo_Tech_YT </b> \n 
+<b>Bot Maintained By: @Termin_a_t_o_r </b> \n 
 """
 
     RENAME_403_ERR = "What Are You Doing? Bruuh!!"
